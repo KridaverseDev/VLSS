00ba2f56647664b272fed9d3b3416b8308298757
vinaybh01 <vinaybh0107@gmail.com>
2024-05-20 16:21:33 +0530

    Updated data

--- src/components/veerashaiva/VeerashaivaQuesData.ts
+++ src/components/veerashaiva/VeerashaivaQuesData.ts
@@ -1,21 +1,20 @@
 export const QuestionData: Array<any> = [
   {
     id: 1,
-    Question: "Where are Lingayath's are located ? ",
+    Question: "Where are Lingayath's are located  ",
     Answer:
       "Lingayats are predominantly found in the Indian state of Karnataka, where they form a significant part of the population and have a profound influence on the region's culture, politics, and society. Beyond Karnataka, Lingayat communities are also located in neighboring states such as Maharashtra, Telangana, Andhra Pradesh, Tamil Nadu, and parts of central India. Due to global migration and the diaspora, Lingayat communities have also established themselves in various countries around the world, including the United States, Canada, Australia, and the United Kingdom, maintaining their cultural and religious practices.      ",
   },
   {
     id: 2,
-    Question: "What are Lingayath's lives like ?",
+    Question: "What are Lingayath's lives like ",
     Answer:
       "Lingayats lead lives deeply rooted in the principles of work as worship (Kayaka) and community service (Dasoha), with a strong emphasis on moral integrity, social justice, and equality. Their devotion to Lord Shiva is expressed through the personal worship of the Ishtalinga, symbolizing a direct and intimate connection with the divine. Education and social reform are highly valued, reflecting the community's commitment to Basavanna's teachings. Lingayats actively participate in religious, cultural, and social activities, fostering a sense of community and continuing the legacy of their 12th-century founder. ",
   },
   {
     id: 3,
-    Question: "What are Lingayath's believes ? ",
+    Question: "What are Lingayath's believes  ",
     Answer:
       "Lingayats believe in a form of monotheism centered on Lord Shiva, whom they worship through the Ishtalinga, a personal lingam symbolizing direct and individual connection with the divine. They reject the traditional Hindu caste system and rituals, emphasizing instead a life of personal piety, ethical conduct, and social equality. Key to their faith are the concepts of Kayaka (work as worship) and Dasoha (selfless service), reflecting their belief in the sanctity of labor and the importance of community welfare. Lingayats also value the teachings encapsulated in the Vachanas, poetic writings that emphasize direct spiritual experience and social reform.",
   },
- 
 ];
